b69819ee847f50c65613bb361cc294da8dfb419e
Jkhall81 <jason.kei.hall@gmail.com>
2023-11-17 13:39:33 -0700

    added psycopg2 dependency.

---
+++ app/app/calc.py
@@ -0,0 +1,7 @@
+'''
+bullshit function to test
+'''
+
+def add(x, y):
+    '''add two numbers and return the result'''
+    return x + y
---
+++ app/app/tests.py
@@ -0,0 +1,18 @@
+'''
+sample tests
+'''
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    '''Test the calc module.'''
+
+    def test_add_numbers(self):
+        '''test adding numbers together'''
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+        
