cfccd3d9e591ec1ed404bb00a0e320d2a74fb618
Jkhall81 <jason.kei.hall@gmail.com>
2023-11-19 11:06:59 -0700

    added drf_spectacular and swagger.

--- app/core/tests/tests_admin.py
+++ app/core/tests/tests_admin.py
@@ -36,3 +36,17 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        '''test the edit user page'''
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        '''test the create user page'''
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
