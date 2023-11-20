1ba3e4fb7ea5414ff0a23fe1b89ffb1d337dd843
Jkhall81 <jason.kei.hall@gmail.com>
2023-11-20 13:21:31 -0700

    adding functionality to the api, working on recipe api now.  everything has test coverage.

--- app/app/settings.py
+++ app/app/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_spectacular',
     'user',
+    'recipe',
 ]
 
 MIDDLEWARE = [
--- app/user/views.py
+++ app/user/views.py
@@ -2,7 +2,7 @@
 Views for the user API
 '''
 
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from user.serializers import (
@@ -20,3 +20,14 @@ class CreateTokenView(ObtainAuthToken):
     '''create a new auth token for user'''
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    '''manage the authenticated user.'''
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        '''retrieve and return the authenticated user'''
+        return self.request.user
