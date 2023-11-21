2a8903d2b5ce610611f25da57ffb2b2d76f64175
Jkhall81 <jason.kei.hall@gmail.com>
2023-11-21 13:37:26 -0700

    finished the tag and recipe api.  everything has complete test coverage.  I have been writing tests for a feature before coding the feature.  swagger documentation and token auth work.  now to start the ingredients API.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,6 +7,11 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(email='user@exmaple.com', password='testpass123'):
+    '''create and return a user'''
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
     '''Testing models, as per the name'''
 
@@ -63,3 +68,10 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        '''test creating a tag is successful'''
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
